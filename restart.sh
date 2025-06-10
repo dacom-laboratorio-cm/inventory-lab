03d962f228e8bbc8f6c956723e4d967c8aa833b9
Iago Carmona <iagoortegacarmona@gmail.com>
2025-06-10 10:56:25 -0300

    fix sh

--- restart.sh
+++ restart.sh
@@ -1,4 +1,4 @@
 sudo docker stop inventory-app-container 
 sudo docker rm inventory-app-container
 sudo docker build -t inventory-app .
-sudo docker run -d -p 5000:5000 --name inventory-app-container inventory-app
+docker run -d --restart unless-stopped -p 5000:5000 --name inventory-app-container inventory-app
